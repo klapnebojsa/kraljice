ec3665a6e921e36950825e4dbaf26b18853e36a3
klapnebojsa <klapnebojsa@gmail.com>
2016-09-08 20:28:31 +0200

    .

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kraljice
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kraljice/core_test.clj
@@ -0,0 +1,7 @@
+(ns kraljice.core-test
+  (:require [clojure.test :refer :all]
+            [kraljice.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
